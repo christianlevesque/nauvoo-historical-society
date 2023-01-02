52330f7990e815c10e71053ae2c957a180f6e88f
Christian LeVesque <christian@levesque.dev>
2023-01-01 18:21:31 -0600

    saving work

--- Server/Tools/Constants/CssClasses.cs
+++ Server/Tools/Constants/CssClasses.cs
@@ -2,7 +2,7 @@ namespace Server.Tools.Constants;
 
 public static class CssClasses
 {
-	public const string Body = "d-flex flex-column full-height";
+	public const string Body = "d-flex flex-column min-vh-100";
 	public const string Main = "flex-grow-1";
 	public const string NarrowMain = $"{Main} my-5";
 }
